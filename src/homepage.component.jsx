26f8d521bf73b97af15512dc795855fcc585cebc
Yurii Sydoriak <sydoriak.yura@gmail.com>
2025-11-19 13:18:39 +0200

    Improve code base

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 
 import './homepage.styles.scss';
 
-const HomePage = () => (
+export const HomePage = () => (
   <div className='homepage'>
     <h1>Homepage</h1>
   </div>
 );
 
-export default HomePage;
