43a499da8c7738f8af9faf1e1e0f4c663727bf57
Yurii Sydoriak <sydoriak.yura@gmail.com>
2026-01-29 17:48:49 +0200

    Changes for HomePage component

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -6,7 +6,10 @@ export const HomePage = () => (
   <div className='homepage'>
     <h1>Homepage component</h1>
 
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque laborum numquam quod consequuntur provident. At iusto vero assumenda ipsum vitae veniam, atque eum reiciendis, illo iure sequi possimus, facere tempora?</p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque laborum numquam quod consequuntur provident.
+      At iusto vero assumenda ipsum vitae veniam, atque eum reiciendis, illo iure sequi possimus, facere tempora?
+    </p>
   </div>
 );
 
