fbc3a2f3b15b428a87bc3619b46924c784c00bbd
Yurii Sydoriak <sydoriak.yura@gmail.com>
2026-01-30 15:56:42 +0200

    Delete dummy text

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -5,11 +5,6 @@ import './homepage.styles.scss';
 export const HomePage = () => (
   <div className='homepage'>
     <h1>Homepage component</h1>
-
-    <p>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque laborum numquam quod consequuntur provident.
-      At iusto vero assumenda ipsum vitae veniam, atque eum reiciendis, illo iure sequi possimus, facere tempora?
-    </p>
   </div>
 );
 
