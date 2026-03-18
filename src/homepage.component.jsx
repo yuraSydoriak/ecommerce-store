4cc984f1dfeca98f94c0e3a730ddeac3deafa5b1
Yurii Sydoriak <sydoriak.yura@gmail.com>
2026-03-18 11:14:22 +0200

    Add changes to homepage component

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -7,6 +7,12 @@ export const HomePage = () => (
     <h1>Homepage</h1>
 
     <p>Welcome to the homepage!</p>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Itaque repudiandae accusamus dicta voluptas eveniet, suscipit laboriosam odio veritatis, minima minus sapiente.
+      Obcaecati eligendi harum est vel alias voluptates totam laudantium!
+    </p>
   </div>
 );
 
