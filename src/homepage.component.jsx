df88563dcb731e5390384160ec87eb732f0845e5
Yurii Sydoriak <sydoriak.yura@gmail.com>
2026-04-03 13:18:39 +0300

    Add content

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -13,6 +13,18 @@ export const HomePage = () => (
       Itaque repudiandae accusamus dicta voluptas eveniet, suscipit laboriosam odio veritatis, minima minus sapiente.
       Obcaecati eligendi harum est vel alias voluptates totam laudantium!
     </p>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Itaque repudiandae accusamus dicta voluptas eveniet, suscipit laboriosam odio veritatis, minima minus sapiente.
+      Obcaecati eligendi harum est vel alias voluptates totam laudantium!
+    </p>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Itaque repudiandae accusamus dicta voluptas eveniet, suscipit laboriosam odio veritatis, minima minus sapiente.
+      Obcaecati eligendi harum est vel alias voluptates totam laudantium!
+    </p>
   </div>
 );
 
