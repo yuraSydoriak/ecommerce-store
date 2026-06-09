442d206ca09a67571cdd0cd4ceb0e63a7a3b0821
Yurii Sydoriak <sydoriak.yura@gmail.com>
2026-06-09 17:52:23 +0300

    Changes

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -5,26 +5,6 @@ import './homepage.styles.scss';
 export const HomePage = () => (
   <div className='homepage'>
     <h1>Homepage</h1>
-
-    <p>Welcome to the homepage!</p>
-
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Itaque repudiandae accusamus dicta voluptas eveniet, suscipit laboriosam odio veritatis, minima minus sapiente.
-      Obcaecati eligendi harum est vel alias voluptates totam laudantium!
-    </p>
-
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Itaque repudiandae accusamus dicta voluptas eveniet, suscipit laboriosam odio veritatis, minima minus sapiente.
-      Obcaecati eligendi harum est vel alias voluptates totam laudantium!
-    </p>
-
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Itaque repudiandae accusamus dicta voluptas eveniet, suscipit laboriosam odio veritatis, minima minus sapiente.
-      Obcaecati eligendi harum est vel alias voluptates totam laudantium!
-    </p>
   </div>
 );
 
